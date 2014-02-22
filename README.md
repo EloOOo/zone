ff0140d3fe1fb73da9bdc5ca42d3984a5d01e85f
Ricain <jeanmercadier@gmail.com>
2014-02-22 12:52:51 +0100

    finalisation generateur + explication readme

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-zone
+Zone
 ====
 
-Projet d'optimisation avec GLPK
+Projet d'optimisation avec GLPK: problème d'activation de capteur pour la surveillance de zones.
+
+[énnoncé](http://raw.github.com/Ricain/zone/projet-plcapteurs.pdf)
+
+# Import
+
+    import random
+    import json
+    import sys
+
+# Réalisation
+
+## Partie 1
+
+Géneration des données du probleme: fichier **genrator.py**
+
+Ce fichier prend deux arguments:
+
+1. `filename` (obligatoire) correspond au nom du fichier dans le quelle seront suavegardé les données generés.
+2. `seed` (optionelle) correspond à la "graine" du generateur du nombre aleatoire.
--- generator.py
+++ generator.py
@@ -12,19 +12,26 @@ MIN_TMP      = 1      # temps de vie minimum d'un capteur
 MAX_TMP      = 10     # temps de bie maximun d'un capteur
 
 if len(sys.argv)<2:
-	print "missing file name"
+	print "usage: filename [seed]"
 	exit(1)
+
+if len(sys.argv)>=3 :
+	seed = int("0" + sys.argv[2])
+else : 
+	seed = random.randint(sys.maxint)
+rand = random.Random(seed)
+print "seed: " + str(seed)
 
 zone = 0
 capteur = {}
 for t in range(NB_CAPTEURS):
-	tmp = random.randint(MIN_TMP,MAX_TMP)
+	tmp = rand.randint(MIN_TMP,MAX_TMP)
 	localzone = []
-	for _ in range(random.randint(MIN_ZONES,MAX_ZONES)):
+	for _ in range(rand.randint(MIN_ZONES,MAX_ZONES)):
 		toadd  = -1
 		while toadd in localzone or toadd==-1 :
-			if random.random()<ZONE_COEF and zone>=MAX_ZONES :
-				toadd = random.randint(1,zone)	
+			if rand.random()<ZONE_COEF and zone>=MAX_ZONES :
+				toadd = rand.randint(1,zone)	
 			else :
 				zone += 1
 				toadd = zone
@@ -35,7 +42,7 @@ print str(len(capteur)) + " capteurs cree"
 print str(zone) + " zones cree"
 
 save = open(sys.argv[1], "w")
-save.write(json.dumps(capteur,indent=4))
+save.write("// seed = " + str(seed) + "\n\n" +json.dumps(capteur,indent=4))
 save.close() 
 
 print "resultat ecrit dans " + sys.argv[1]
