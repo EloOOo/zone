ec5108ae955833da7d0521fb5095aaf905ee57c2
Ricain <jeanmercadier@live.com>
2014-03-03 14:25:47 +0100

    revue des arguement et des constantes

--- generator.py
+++ generator.py
@@ -4,33 +4,49 @@ import random
 import json
 import sys
 
-NB_CAPTEURS  = 10    # nombre de capteurs
-MIN_ZONES    = 1      # nombre minimun de zone par capteur
-MAX_ZONES    = 4     # nombre maximum de zone par capteur
-ZONE_COEF    = .84    # attribution de zone deja cree 0 < ZONE_COEF <= 1 attribution de nouvelle zone
-MIN_TMP      = 1      # temps de vie minimum d'un capteur
-MAX_TMP      = 10     # temps de bie maximun d'un capteur
+const = {}
+const["nbcapteur"]  = 10    # nombre de capteurs
+const["minzone"]    = 1     # nombre minimun de zone par capteur
+const["maxzone"]    = 4     # nombre maximum de zone par capteur
+const["zonecoef"]   = .84   # attribution de zone deja cree 0 < const["zonecoef"] <= 1 attribution de nouvelle zone
+const["mintmp"]     = 2     # temps de vie minimum d'un capteur
+const["maxtmp"]     = 10    # temps de vie maximun d'un capteur
+const["seed"]       = 0     # graine du generateur de nombre aleatoire, zero pour choisi aleatoirement
 
 if len(sys.argv)<2:
-	print "usage: filename [seed]"
+	print "usage: filename "
+	for x,y in const.iteritems():
+		if type(y) is int:
+			print "[-" + x + " <int>]"
+		else :
+			print "[-" + x + " <float>]"
 	exit(1)
 
-if len(sys.argv)>=3 :
-	seed = int("0" + sys.argv[2])
-else : 
-	seed = random.randint(0,sys.maxint)
-rand = random.Random(seed)
-print "seed: " + str(seed)
+for arg in sys.argv[2:]:
+	if arg[:1]=="-" :
+		if arg[1:] in const:
+			if type(const[arg[1:]]) is int:
+				const[arg[1:]] = int(sys.argv[sys.argv.index(arg)+1])
+			else :
+				const[arg[1:]] = float(sys.argv[sys.argv.index(arg)+1])
+		else :
+			print arg[1:] + ": illegal option"
+			exit(2)
+
+if const["seed"] == 0:
+	const["seed"] = random.randint(0,sys.maxint)
+rand = random.Random(const["seed"])
+print "seed: " + str(const["seed"])
 
 zone = 0
 capteur = {}
-for t in range(NB_CAPTEURS):
-	tmp = rand.randint(MIN_TMP,MAX_TMP)
+for t in range(const["nbcapteur"]):
+	tmp = rand.randint(const["mintmp"],const["maxtmp"])
 	localzone = []
-	for _ in range(rand.randint(MIN_ZONES,MAX_ZONES)):
+	for _ in range(rand.randint(const["minzone"],const["maxzone"])):
 		toadd  = -1
 		while toadd in localzone or toadd==-1 :
-			if rand.random()<ZONE_COEF and zone>=MAX_ZONES :
+			if rand.random()<const["zonecoef"] and zone>=const["maxzone"] :
 				toadd = rand.randint(1,zone)	
 			else :
 				zone += 1
