b8581f12f082bd7b45d2a06b536183f4028641e1
Ricain <jeanmercadier@live.com>
2014-03-03 11:22:31 +0100

    changement constante

--- generator.py
+++ generator.py
@@ -4,9 +4,9 @@ import random
 import json
 import sys
 
-NB_CAPTEURS  = 100    # nombre de capteurs
+NB_CAPTEURS  = 10    # nombre de capteurs
 MIN_ZONES    = 1      # nombre minimun de zone par capteur
-MAX_ZONES    = 10     # nombre maximum de zone par capteur
+MAX_ZONES    = 4     # nombre maximum de zone par capteur
 ZONE_COEF    = .84    # attribution de zone deja cree 0 < ZONE_COEF <= 1 attribution de nouvelle zone
 MIN_TMP      = 1      # temps de vie minimum d'un capteur
 MAX_TMP      = 10     # temps de bie maximun d'un capteur
